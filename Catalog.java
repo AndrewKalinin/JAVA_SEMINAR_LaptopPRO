2c7fa339d6dbc1164df340fb7946e6a9d7fde658
AndrewKalinin <96065838+AndrewKalinin@users.noreply.github.com>
2023-04-13 21:12:25 +0300

    Add files via upload

---
+++ Catalog.java
@@ -0,0 +1,10 @@
+package LaptopPRO;
+
+
+public class Catalog extends Laptop {
+
+    public Catalog(int id, int ram, int ssd, String os, String color){
+        super(id,ram,ssd,os,color);
+    }
+ 
+}
